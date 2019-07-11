6ca19fc1521091703493274a56d92f3e6a510fca
ketan1410 <47491528+ketan1410@users.noreply.github.com>
2019-07-11 19:51:16 +0530

    Using Firefox instead of chrome

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -26,13 +26,13 @@ public class Hook extends BaseUtil{
 
         System.out.println("Opening the browser : Firefox");
 
-        /*System.setProperty("webdriver.firefox.marionette", "C:\\Libs\\geckodriver.exe");
-        base.Driver = new FirefoxDriver();*/
+        System.setProperty("webdriver.firefox.marionette", "C:\\Libs\\geckodriver.exe");
+        base.Driver = new FirefoxDriver();
 
 
-#        //Chrome driver
-#        System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
-#        base.Driver = new ChromeDriver();
+        //Chrome driver
+        /*System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
+        base.Driver = new ChromeDriver();*/
     }
 
 
