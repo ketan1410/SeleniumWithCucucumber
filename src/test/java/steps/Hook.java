30c0ec2f3b72defd111554af5bf9520861d18a16
ketan1410 <47491528+ketan1410@users.noreply.github.com>
2019-12-13 20:15:38 +0530

    Reverted Chnages

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -27,7 +27,7 @@ public class Hook extends BaseUtil{
         System.out.println("Opening the browser : Firefox");
 
         /*System.setProperty("webdriver.firefox.marionette", "C:\\Libs\\geckodriver.exe");
-        base.Driver = new FirefoxDriver();
+        base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
