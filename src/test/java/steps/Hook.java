7016b592a4b6d83f5426c34634f7720a504ab71f
ketan1410 <47491528+ketan1410@users.noreply.github.com>
2020-01-16 18:16:00 +0530

    Updated Comments

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -29,8 +29,6 @@ public class Hook extends BaseUtil{
         /*System.setProperty("webdriver.firefox.marionette", "C:\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
 
-
-        //Chrome driver
         System.setProperty("webdriver.chrome.driver", "C:\chromedriver\chromedriver.exe");
         base.Driver = new ChromeDriver();
     }
